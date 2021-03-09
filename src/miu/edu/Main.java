dd73816a0d1a564a157c3c390fd6e2eca1b1f8d9
phuocdoan <ddphuoc@gmail.com>
2021-03-09 01:13:36 -0600

    09-Mar-2021

--- src/miu/edu/Main.java
+++ src/miu/edu/Main.java
@@ -77,4 +77,70 @@ public class Main {
         return result;
     }
 
+    /*
+    *   Write a function to find the longest common prefix string amongst an array of strings.
+    *   If there is no common prefix, return an empty string "".
+    *   Example 1:
+    *       Input: strs = ["flower","flow","flight"]
+    *       Output: "fl"
+    *   Example 2:
+    *       Input: strs = ["dog","racecar","car"]
+    *       Output: ""
+    *       Explanation: There is no common prefix among the input strings.
+    *
+    *   Constraints:
+    *       0 <= strs.length <= 200
+    *       0 <= strs[i].length <= 200
+    *       strs[i] consists of only lower-case English letters.
+     */
+    public static String longestCommonPrefix(String[] strs) {
+        String s = "";
+        for(int i = 0; i <= strs.length - 1; i++) {
+            if(strs[i].equals(""))
+                return "";
+            if(s.equals(""))
+                s = strs[i];
+            else {
+                int j = Math.min(s.length(), strs[i].length());
+                while (j >= 1 && !strs[i].startsWith(s.substring(0, j))) {
+                    j--;
+                }
+                if(j == 0)
+                    return "";
+                else {
+                    s = s.substring(0,j);
+                }
+            }
+        }
+        return s;
+    }
+
+    /*
+    *   Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+    *   Example 1:
+    *       Input: l1 = [1,2,4], l2 = [1,3,4]
+    *       Output: [1,1,2,3,4,4]
+    *   Example 2:
+    *       Input: l1 = [], l2 = []
+    *       Output: []
+    *   Example 3:
+    *       Input: l1 = [], l2 = [0]
+    *       Output: [0]
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
